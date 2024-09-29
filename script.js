9a8b9cdc0dac6bcab7297a895d4e37d52225fdbf
sujipinku07 <sujipinku07@gmail.com>
2024-09-29 16:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function insert_Row() {
+        let table = document.getElementById("sampleTable");
+        let newRow = table.insertRow(0);
+
+        let cell1 = newRow.insertCell(0);
+        cell1.innerHTML = "New Cell1";
+
+        let cell2 = newRow.insertCell(1);
+        cell2.innerHTML = "New Cell2";
+      }
